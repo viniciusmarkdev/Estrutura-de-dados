114ab5bf2cc987069b3a443d34bde3868de1e672
Marcos Vinicius <contato.marcoscandido@gmail.com>
2023-04-11 16:37:21 -0300

    SelectionSort

---
+++ src/SelectionSort.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class SelectionSort {
+	
+	
+	public static void main(String[] args) {
+		
+		int [] vetor = {2,1};
+		
+		
+		ordenacaoPorSelecao(vetor);
+		
+		for(int i=0; i <vetor.length;i++) {
+			
+			System.out.println(vetor[i]);
+		}
+	
+	}
+	
+	
+	
+	public static void ordenacaoPorSelecao(int [] arr) {
+		
+		
+		int menorIndice , aux;
+		
+		
+		for(int i=0 ; i<arr.length ; i++) {
+			
+			menorIndice = i;
+			
+             for(int j=1+i; j<arr.length ; j++) {
+			
+			
+			
+				
+				if(arr[j]<arr[menorIndice]) {
+					
+					menorIndice = j;
+					
+					
+					
+				
+				
+			}
+			
+		}
+             
+         
+         aux = arr[menorIndice];
+         
+         arr[menorIndice] = arr[i];
+         
+         arr[i] = aux;
+         
+         
+       
+             
+       
+			
+		
+             
+				
+			}
+		
+		
+		  
+		
+		
+		
+		
+	}
+	
+
+}
